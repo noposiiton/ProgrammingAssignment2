f1b30e43864c3ab8343db84f270af2701e448af0
noposition <marcus.scott@gmail.com>
2015-05-21 11:31:35 -0400

    Updated cachematrix
    Assignment upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates a special vector such that the values are set in a different environment
+## to take advantage of scoping rules.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## If this matrix has a non-NULL value for getinv(), then cacheSolve simply returns that value.
+## If the matrix has a NULL value for getinv() then cacheSolve calculates and stores the inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinv(i)
+    i
 }
